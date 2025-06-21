76cb63959ccbf5fd752953535b82030b3c8ecb25
Mohammed Sinan KH <89580314+Mohd-Sinan@users.noreply.github.com>
2025-06-21 17:15:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = Path("README.md").read_text(encoding="utf-8").replace("<pre>", "# Maudio - Minimal Morse code audio encoder\n").split("</pre>")[-1].lstrip()
 
-giturl=""
+giturl="https://github.com/Mohd-Sinan/maudio"
 
 setup(
       name='maudio',
       version='0.0.1',
       author='Mohammed Sinan KH',
       maintainer='Mohammed Sinan KH',
-      author_email='author@example.com',
+      author_email='instasinan123@gmail.com',
       license='MIT',
-      description='A sample Python project',
+      description='Minimal Morse code audio encoder ( WAV format )',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url=giturl,
@@ -30,7 +30,7 @@ setup(
       'Programming Language :: Python :: 3.8',
       'Programming Language :: Python :: 3.9',
       ],
-      keywords='morse code audio',
+      keywords='morse code audio generation',
       python_requires='>=3.6',
 
       entry_points={
